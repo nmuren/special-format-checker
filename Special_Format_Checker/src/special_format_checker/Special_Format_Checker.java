1c13c311bca57882bf18fd5c924112af6b94012a
Nebil Muren <nebilmuren@gmail.com>
2019-11-30 01:32:51 -0500

    Update Special_Format_Checker.java

--- Special_Format_Checker/src/special_format_checker/Special_Format_Checker.java
+++ Special_Format_Checker/src/special_format_checker/Special_Format_Checker.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 final class Stack {
-    
+
     private ArrayList<Character> charList;
     private boolean correct;
-    
+
     Stack(String s) {
         charList = new ArrayList<>();
         correct = true;
         Character c, popped;
         for (int i = 0; i < s.length(); i++) {
             c = s.charAt(i);
-            
+
             switch (c) {
                 case '[':
                 case '{':
                 case '(':
                     push(c);
                     break;
-                
+
                 case ')':
                     popped = pop();
                     if (!popped.equals('(')) {
@@ -43,30 +43,30 @@ final class Stack {
             }
         }
         if (!charList.isEmpty()) {
-            correct=false;
+            correct = false;
         }
     }
-    
+
     private void push(char c) {
         charList.add(c);
     }
-    
+
     private char pop() {
         return charList.remove(charList.size() - 1);
     }
-    
+
     boolean isCorrect() {
         return correct;
     }
-    
+
     public void setCorrect(boolean correct) {
         this.correct = correct;
     }
-    
+
 }
 
 public class Special_Format_Checker {
-    
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
@@ -76,13 +76,13 @@ public class Special_Format_Checker {
             stack = new Stack(s);
             if (stack.isCorrect()) {
                 System.out.println("Your entry is a mirror, congratulations!");
-            }else{
+            } else {
                 System.out.println("Sorry, your entry is not a mirror string.");
             }
         } catch (Exception e) {
             System.out.println("Sorry, your entry is not a mirror string.");
         }
-        
+
     }
-    
+
 }
