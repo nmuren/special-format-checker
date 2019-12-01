333d88d483c2c0e94b79ba3e447946e3ddbb96ae
Nebil Muren <nebilmuren@gmail.com>
2019-11-30 22:06:28 -0500

    Added LinkedList type Stack

--- Special_Format_Checker/src/special_format_checker/Special_Format_Checker.java
+++ Special_Format_Checker/src/special_format_checker/Special_Format_Checker.java
@@ -1,6 +1,7 @@
 package special_format_checker;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 final class Stack {
@@ -41,6 +42,9 @@ final class Stack {
                     }
                     break;
             }
+            if (!isCorrect()) {
+                break;
+            }
         }
         if (!charList.isEmpty()) {
             correct = false;
@@ -61,15 +65,78 @@ final class Stack {
 
 }
 
+final class StackWith_LL {
+
+    private LinkedList stack;
+    private Boolean correct;
+
+    StackWith_LL(String s) {
+        stack = new LinkedList<>();
+        correct = true;
+        Character c, popped;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+
+            switch (c) {
+                case '[':
+                case '{':
+                case '(':
+                    push(c);
+                    break;
+
+                case ')':
+                    popped = pop();
+                    if (!popped.equals('(')) {
+                        correct = false;
+                    }
+                    break;
+                case '}':
+                    popped = pop();
+                    if (!popped.equals('{')) {
+                        correct = false;
+                    }
+                    break;
+                case ']':
+                    popped = pop();
+                    if (!popped.equals('[')) {
+                        correct = false;
+                    }
+                    break;
+            }
+            if (!isCorrect()) {
+                break;
+            }
+        }
+        if (!stack.isEmpty()) {
+            correct = false;
+        }
+    }
+
+    private void push(Character c) {
+        stack.addLast(c);
+    }
+
+    private Character pop() {
+        return (Character) stack.removeLast();
+    }
+
+    boolean isCorrect() {
+        return correct;
+    }
+
+}
+
 public class Special_Format_Checker {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        //String s = "{[[((7-9)*(4+5)(4^2)[])]][]}";
-        Stack stack;
+        /*Scanner in = new Scanner(System.in);
+        String s = in.nextLine();*/
+        String s = "{[[((7-9)*(4+5)(4^2)[])]][]}";
+        //Stack stack;
+        StackWith_LL stack;
         try {
-            stack = new Stack(s);
+            //stack = new Stack(s);
+            stack = new StackWith_LL(s);
             if (stack.isCorrect()) {
                 System.out.println("Your entry is a mirror, congratulations!");
             } else {
